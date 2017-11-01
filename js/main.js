d41f748625a7c1eefa24ace793eb101aa4c40b80
Kaamos88 <32678958+Kaamos88@users.noreply.github.com>
2017-11-01 19:24:09 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(".one, .two, .three, .four, #skills, #Contact").hide();
+$(document).ready(function(){
+    $(".one").delay(500).fadeIn(4000);
+    $(".two").delay(500).fadeIn(4000);
+    $(".three").delay(500).fadeIn(5000);
+    $(".four").delay(500).fadeIn(5000);
+});
