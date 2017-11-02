4eaa619c2f46382b191d74c4be3a63d3661fbd7e
Kaamos88 <32678958+Kaamos88@users.noreply.github.com>
2017-11-02 12:52:45 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
-$(".one, .two, .three, .four, #skills, #Contact").hide();
+$(".one, .two, .three, .four, #lang").hide();
 $(document).ready(function(){
     $(".one").delay(500).fadeIn(4000);
     $(".two").delay(500).fadeIn(4000);
     $(".three").delay(500).fadeIn(5000);
     $(".four").delay(500).fadeIn(5000);
+    $("#lang").delay(500).fadeIn(5000);
+});
+
+$(document).ready(function(){
+    $("#pl").click(function(){
+        $(".en").hide();
+        $(".pl").show();
+    });
+    $("#en").click(function(){
+        $(".en").show();
+        $(".pl").hide();
+    });
 });
